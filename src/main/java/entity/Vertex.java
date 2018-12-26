106500cccd793650a8df2a0e71c08f343a13acd7
江天 <jt2594838@163.com>
2018-12-26 21:37:28 +0800

    fix edge usage in vertex

--- src/main/java/entity/Vertex.java
+++ src/main/java/entity/Vertex.java
@@ -1,9 +1,39 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 public class Vertex {
     public int id;
 
-    public Map<Integer, Edge> outEdges;
+    private Map<Integer, Edge> outEdges;
+
+    private List<Vertex> neighbors;
+
+    public List<Vertex> getNeighbors() {
+        if (neighbors == null) {
+            neighbors = new ArrayList<>();
+            for(Edge e : outEdges.values()) {
+                if(e.from.id == id)
+                    neighbors.add(e.to);
+                else
+                    neighbors.add(e.from);
+            }
+        }
+        return neighbors;
+    }
+
+    public int degree() {
+        return outEdges.size();
+    }
+
+    public Collection<Edge> getEdges() {
+        return outEdges.values();
+    }
+
+    public Edge getEdge(int v) {
+        return outEdges.get(v);
+    }
 }
